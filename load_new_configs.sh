61421947bb7927ad4bae3f5c222f0738c2d73a5c
Iwwww <mike.yolgin@gmail.com>
2022-04-27 21:29:32 +0300

    Added bash script for loading dot files 	изменено:      load_new_configs.sh

--- load_new_configs.sh
+++ load_new_configs.sh
@@ -5,11 +5,5 @@
 i3ConfigNewFile="$HOME/.i3/config"
 i3ConfigOldFile="$HOME/Documents/i3-sway-config/i3/config"
 
-if diff -s "$i3ConfigNewFile" "$i3ConfigOldFile"; then
-    printf 'The file "%s" is the same as "%s"\n' "$i3ConfigNewFile" "$i3ConfigOldFile"
-else
-    printf 'Backup old file and copy new to a new file\n'
-    #mv -b "$i3ConfigOldFile" "$i3ConfigOldFile"
-    cp -b $i3ConfigNewFile $i3ConfigOldFile
-fi
-
+cp --backup=numbered "$i3ConfigOldFile" "i3/config.bak"
+cp "$i3ConfigNewFile" "$i3ConfigOldFile"
