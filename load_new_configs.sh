693a399691ea544bdb0a0e34d03fe3f4a3f04e46
Iwwww <mike.yolgin@gmail.com>
2022-05-01 12:37:47 +0300

    Edit load_new_configs.sh, added '-v' flag to 'cp' command

--- load_new_configs.sh
+++ load_new_configs.sh
@@ -5,11 +5,11 @@
 i3ConfigNewFile="$HOME/.i3/config"
 i3ConfigOldFile="$HOME/Documents/i3-sway-config/i3/config"
 
-cp --backup=numbered "$i3ConfigOldFile" "i3/config.bak"
-cp "$i3ConfigNewFile" "$i3ConfigOldFile"
+cp --backup=numbered -v "$i3ConfigOldFile" "i3/config.bak"
+cp -v "$i3ConfigNewFile" "$i3ConfigOldFile"
 
-cp --backup=numbered "dunst/dunstrc" "dunst/dunstrc.bak"
-cp "$HOME/.config/dunst/dunstrc" "dunst/dunstrc"
+cp --backup=numbered -v "dunst/dunstrc" "dunst/dunstrc.bak"
+cp -v "$HOME/.config/dunst/dunstrc" "dunst/dunstrc"
 
 cp --backup=numbered -v "picom/picom.conf" "picom/picom.conf.bak"
 cp -v "$HOME/.config/picom.conf" "picom/picom.conf"
