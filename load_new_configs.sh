2a8c9d24dc45321bef9b224ffd4c7f6facad8879
Iwwww <mike.yolgin@gmail.com>
2022-05-29 16:52:03 +0300

    Move picom.conf to picom/picom.conf

--- load_new_configs.sh
+++ load_new_configs.sh
@@ -12,7 +12,7 @@ cp --backup=numbered -v "dunst/dunstrc" "dunst/dunstrc.bak"
 cp -v "$HOME/.config/dunst/dunstrc" "dunst/dunstrc"
 
 cp --backup=numbered -v "picom/picom.conf" "picom/picom.conf.bak"
-cp -v "$HOME/.config/picom.conf" "picom/picom.conf"
+cp -v "$HOME/.config/picom/picom.conf" "picom/picom.conf"
 
 cp -rv --backup "$HOME/.config/lf/" "."
 
