8357c85449a4c3b234355438f3a320645db11f7e
Iwwww <m.elgin@alarmtrade.ru>
2026-03-20 13:15:38 +0300

    Delete old files

--- .local/scripts/run_morsetrainer.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-LANG="ru_RU.UTF-8" wine ~/Documents/winApps/MorzeTrainer_281213.exe
--- .local/scripts/set_governor.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-# Set governor
-for file in /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor; do echo "$1" > $file; done
